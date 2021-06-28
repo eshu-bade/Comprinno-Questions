41ca0a1fa30d60e7e8ff32ac94eae3e55ee36b24
Eswar Kumar <eswar.bade62@gmail.com>
2021-06-28 11:08:26 +0530

    Update Question6.py

--- Question6.py
+++ Question6.py
@@ -5,11 +5,10 @@ def CatsDogs():
         cat = each[0]
         dog = each[1]
         legs = each[-1]
-        if cat ==1 and (sum(each[:2])*4 == legs or sum(each[:2])*2 == legs):
+        animalsCount = sum(each[:1]) # without the legs count
+        if dog ==1 and cat <=3 and (legs in [i*4 for i in range(dog, animalsCount+1)] or legs == 8):
             print("YES")
-        elif (cat == 2 and dog == 1) and (legs ==4 or legs == 8 or legs == 12):
-            print("YES")
-        elif dog == 2 and (legs == 8 or legs == 12 or legs == 16 or legs == 20):
+        elif dog == 2 and cat>=4 and cat <= 6 and (legs in [i*4 for i in range(dog, animalsCount+1)]):
             print("YES")
         else:
             print("NO")
@@ -18,7 +17,7 @@ def CatsDogs():
 testCases = int(input("testCases are: "))
 full_list = []
 for i in range(testCases):
-    pairs = list(map(int, input("Enter cats, dogs and legs counts by separating with space : ").strip().split(' ')))[:3]
+    pairs = list(map(int, input("Enter numbers by separating with space : ").strip().split(' ')))[:3]
     full_list.append(pairs)
 
 
